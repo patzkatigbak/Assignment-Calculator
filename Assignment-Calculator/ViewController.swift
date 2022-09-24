1e407dc9546c52ed7734b9848bfcfcdaa4031dd7
patzkatigbak <patzkatigbak@gmail.com>
2022-09-23 20:31:50 -0400

    Sudent ID update
    added student ID

--- Assignment-Calculator/ViewController.swift
+++ Assignment-Calculator/ViewController.swift
@@ -1,7 +1,7 @@
 /*
  File Name: Assignment-Calculator
  Author:Patrick Katigbak, Sao Kuan I Chan Yat Man
- Student ID: 301253113, 301204757
+ Student ID: 301253113, 301204757, 301279592
  Date: 2022-09-19
  App Description: UI of Calculator without logic. Combination of View, Stacks, Button, Color, label
  */
